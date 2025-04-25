bcb78c626d8bc3d9cc0c793f87fa712a0c87e1b5
Kempek <egar.kill@gmail.com>
2025-04-25 21:43:39 +0300

    headers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
 import { useTelegram } from './hooks/useTelegram'
 function App() {
 	const { onToggleButton, tg } = useTelegram()
@@ -8,6 +9,7 @@ function App() {
 	}, [])
 	return (
 		<div className='App'>
+			<Header />
 			<button onClick={onToggleButton}>toogle</button>
 		</div>
 	)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,4 +11,4 @@ const Header = () => {
 		</div>
 	)
 }
-export default Button
+export default Header
