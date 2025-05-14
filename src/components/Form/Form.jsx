3350bd1b708b72e6c09b8a4cca03704d80fdf231
Kempek <egar.kill@gmail.com>
2025-05-15 01:00:42 +0300

    Обработка данных sendData обратно пользователю street,city и т.д.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useTelegram } from '../../hooks/useTelegram'
 import './Form.css'
 function Form() {
@@ -7,6 +7,20 @@ function Form() {
 	const [subject, setSubject] = useState('physical')
 	const [street, setStreet] = useState('')
 	const { tg } = useTelegram()
+	const onSendData = useCallback(() => {
+		const data = {
+			country,
+			street,
+			subject,
+		}
+		tg.onSendData(JSON.stringify(data))
+	}, [])
+	useEffect(() => {
+		tg.onEvent('mainButtonClicked', onSendData)
+		return () => {
+			tg.offEvent('mainButtonClicked', onSendData)
+		}
+	}, [])
 	useEffect(() => {
 		tg.MainButton.setParams({
 			text: 'Отправить данные',
