fafe41c71229d25c228cc470224b62ffe2515232
Kempek <egar.kill@gmail.com>
2025-05-15 02:11:11 +0300

    фикс ошибок

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,7 +14,7 @@ function Form() {
 			subject,
 		}
 		tg.sendData(JSON.stringify(data))
-	}, [])
+	}, [country, street, subject])
 	useEffect(() => {
 		tg.onEvent('mainButtonClicked', onSendData)
 		return () => {
